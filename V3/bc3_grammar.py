4d39fbe9de5b7e97a13f7df19b70a91fa51b2f00
nagyj2 <jaysun_n@hotmail.com>
2021-02-24 23:01:36 -0500

    Added skeleton parser

---
+++ V3/bc3_grammar.py
@@ -0,0 +1,68 @@
+# NGL Bytecode 3.0 Grammar Skeleton
+
+from bc3_scanner import Scanner
+from bc3_scanner import ENOT, PARAM, OR, UNION, AND, INTER, EQ, NE, LT, GT, PLUS, MINUS, MULT, DIV, INTDIV, MOD, EXP, NOT, CAST, VAR, CONST, READ, SET, DEL, GOTO, IF, TRY, EXEC, PRINT, INCLUDE, QUIT, RETURN, RAISE, LOG, GOARROW1, GOARROW2, RETARROW1, RETARROW2, ARROWSHAFT, NOJUMP, LPAREN, RPAREN, LBRAK, RBRAK, LCURLY, RCURLY, COMMA, COLON, LINEEND, INT, FLOAT, STR, BOOL, ARRAY, LIST, NONE, NUMBER, DECIMAL, STRING, IDENT, EOF, ARROWS, TYPES
+import bc3_symboltable as ST
+
+def prog():
+    pass
+
+def line():
+    pass
+
+def label():
+    pass
+
+def stmt():
+    pass
+
+def expr():
+    pass
+
+def expr_l0():
+    pass
+
+def expr_l1():
+    pass
+
+def expr_l2():
+    pass
+
+def expr_l3():
+    pass
+
+def expr_l4():
+    pass
+
+def expr_l5():
+    pass
+
+def expr_l6():
+    pass
+
+def expr_l7():
+    pass
+
+def expr_l8():
+    pass
+
+def expr_l9():
+    pass
+
+def subatom():
+    pass
+
+def cast():
+    pass
+
+def typ():
+    pass
+
+def primitive():
+    pass
+
+def collection():
+    pass
+
+def lineend():
+    pass
