1475ab4eeb252670f52a3d3e3eb814491cef14dd
nagyj2 <jaysun_n@hotmail.com>
2021-02-26 22:00:47 -0500

    improved logging a bit

--- V3/bc3_logging.py
+++ V3/bc3_logging.py
@@ -3,6 +3,10 @@
 
 import logging
 
+# TODO: move all logging to functions originating here
+# i.e. get rid of mark!!
+# TODO: for type checks, put all type errors into the logs
+
 open = []
 
 class LoggerDummy:
@@ -23,19 +27,21 @@ def _initLogger(category,masterLevel,fileLevel,streamLevel,filename):
     # Creates and sets up a logging object and then returns it
     logger = logging.getLogger(category)
     logger.setLevel(masterLevel)
-    fh = logging.FileHandler('_{0}.log'.format(filename),'w') # handler for log file
+    mode = 'w' if len(open) == 0 else 'a' # for debugging, easier to have 1 log
+    fh = logging.FileHandler('_{0}.log'.format(filename),mode) # handler for log file
     fh.setLevel(fileLevel)
     sh = logging.StreamHandler() # handler for console stream
     sh.setLevel(streamLevel)
-    formatter = logging.Formatter('%(asctime)s - %(name)s : %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     sh.setFormatter(formatter)
     logger.addHandler(fh)
     logger.addHandler(sh)
+    open.append(category)
     logger.info('==Started logging {0}==='.format(category))
     return logger
 
-def getLogger(category='master',masterLevel=logging.INFO,fileLevel=logging.INFO,streamLevel=logging.WARNING,filename='master'):
+def getLogger(category='master',masterLevel=logging.INFO,fileLevel=logging.INFO,streamLevel=logging.CRITICAL,filename='master'):
     # If logger is not open, it is initialized. If it is open, simply return
     # instance of respective logger
     global open
@@ -46,7 +52,6 @@ def getLogger(category='master',masterLevel=logging.INFO,fileLevel=logging.INFO,
     elif category in open:
         return logging.getLogger(category)
 
-    open.append(category)
     return _initLogger(category,masterLevel,fileLevel,streamLevel,filename)
 
 if __name__ == '__main__':
